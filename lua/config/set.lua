8a0787aed4df0b4be4e5acfad212050c71d29ddd
Basti <biervertrieb@googlemail.com>
2025-08-03 18:18:40 +0200

    initial

---
+++ lua/config/set.lua
@@ -0,0 +1,8 @@
+vim.g.mapleader = " "
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- line numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
