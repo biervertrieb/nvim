b65bfe2e41f44765772ba6d5f24593b2219a1b5e
Basti <biervertrieb@googlemail.com>
2025-08-11 14:15:01 +0200

    <cmd> needs to end with <CR>

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -8,6 +8,7 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 vim.keymap.set("n", "<leader>y", '"+y', { desc = "[y]ank to clipboard" })
 vim.keymap.set("v", "<leader>y", '"+y', { desc = "[y]ank to clipboard" })
